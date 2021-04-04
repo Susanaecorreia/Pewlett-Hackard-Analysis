e8ce5a7244e07a86a225c448cecf8b73ad2284db
Susanaecorreia <78492568+Susanaecorreia@users.noreply.github.com>
2021-04-04 10:49:30 -0400

    challenge2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -33,3 +33,22 @@ GROUP BY title
 ORDER BY "count" DESC
 
 SELECT * FROM unique_titles
+
+--CHALLENGE 2
+
+SELECT DISTINCT ON (employees.emp_no)employees.emp_no,
+	employees.first_name,
+	employees.last_name,
+	employees.birth_date,
+	dept_emp.from_date,
+	dept_emp.to_date,
+	titles.title
+INTO mentorship_eligibility
+FROM employees
+INNER JOIN dept_emp
+ON (employees.emp_no = dept_emp.emp_no)
+INNER JOIN titles
+ON (employees.emp_no = titles.emp_no)
+WHERE (employees.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY employees.emp_no ASC
+
